91a0572ff621fd2ee5a1c482ff823dae9f6a641c
Curser <331257394@qq.com>
2016-09-02 13:53:48 +0800

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+$(function() {
+$("nav").css("backgroundColor","#008080");
+$("input").css({"border":"0px","outline":"none"});
+$(".bottom-btn").css({"padding":"10px 0px 0px 0px","font-size":"25px","color":"white"});
+$(".messa,.address,.search,.bbs").css({"color":"white","font-size":"20px"});
+$(".home-img").css({"margin":"150px 0 0 0"});
+$(".sent-message").css({"border":"0px","border-radius":"50px","backgroundColor":"#008080","color":"white","width":"250px","font-size":"20px"});
+$(".title").css({"margin":"50px 0 0 0px","font-size":"20px","backgroundColor":"white","width":"100%"});
+$(".title2").css({"margin":"10px 0 0 0"});
+$(".area").css({"margin":"110px 0 0 0px"});
+$(".post").css({"margin":"10px 0 10px 0px"});
+$("p").css({"margin":"0px 0 5px 0px"});
+$(".text").css({"margin":"90px 0 60px 0px"});
+$(".sent").css({"color":"white","margin":"10px 0 10px 0","font-size":"20px"});
+$(".name").css("margin","10px 25px 15px 25px");
+$(".unit").css("margin","10px 10px 15px 0px");
+$(".target").css({"width":"60px"});
+$(".input").css({"width":"60px"});
+$(".data").css({"float":"right","font-size":"15px"});
+$(".mine").css({"width":"180px","font-size":"20px","padding":"0"});
+$(".android-ch").css({"height":"70px","padding":"20px 0 0 0","backgroundColor":"#008080","color":"white"});
+$(".android").css({"margin":"80px 0 30px 0px"});
+
+  });
